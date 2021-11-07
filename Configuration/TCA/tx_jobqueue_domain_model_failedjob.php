ff8fb6eb58c728813d3114be29fd4e475dfa0623
Sven Wappler <info@wappler.systems>
2021-11-08 00:08:59 +0100

    convert array to short syntax

--- Configuration/TCA/tx_jobqueue_domain_model_failedjob.php
+++ Configuration/TCA/tx_jobqueue_domain_model_failedjob.php
@@ -1,62 +1,62 @@
 <?php
-return array(
-    'ctrl' => array(
+return [
+    'ctrl' => [
         'title' => 'LLL:EXT:jobqueue/Resources/Private/Language/locallang_db.xlf:tx_jobqueue_domain_model_failedjob',
         'label' => 'crdate',
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'dividers2tabs' => true,
 
-        'enablecolumns' => array(
+        'enablecolumns' => [
 
-        ),
+        ],
         'searchFields' => 'crdate,queue_name,payload',
         'iconfile' => 'EXT:jobqueue/Resources/Public/Icons/tx_jobqueue_domain_model_failedjob.gif',
         'readOnly' => true,
         'rootLevel' => 1,
-    ),
-    'interface' => array(
+    ],
+    'interface' => [
         'showRecordFieldList' => 'crdate, queue_name, payload',
-    ),
-    'types' => array(
-        '1' => array('showitem' => 'crdate, queue_name, payload'),
-    ),
-    'palettes' => array(
-        '1' => array('showitem' => ''),
-    ),
-    'columns' => array(
+    ],
+    'types' => [
+        '1' => ['showitem' => 'crdate, queue_name, payload'],
+    ],
+    'palettes' => [
+        '1' => ['showitem' => ''],
+    ],
+    'columns' => [
 
-        'crdate' => array(
+        'crdate' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.creationDate',
-            'config' => array(
+            'config' => [
                 'readOnly' => 1,
                 'type' => 'input',
                 'size' => 30,
                 'eval' => 'trim,datetime'
-            ),
-        ),
-        'queue_name' => array(
+            ],
+        ],
+        'queue_name' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue/Resources/Private/Language/locallang_db.xlf:tx_jobqueue_domain_model_failedjob.queue_name',
-            'config' => array(
+            'config' => [
                 'readOnly' => 1,
                 'type' => 'input',
                 'size' => 30,
                 'eval' => 'trim,required'
-            ),
-        ),
-        'payload' => array(
+            ],
+        ],
+        'payload' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue/Resources/Private/Language/locallang_db.xlf:tx_jobqueue_domain_model_failedjob.payload',
-            'config' => array(
+            'config' => [
                 'readOnly' => 1,
                 'type' => 'text',
                 'cols' => 40,
                 'rows' => 15,
                 'eval' => 'trim,required'
-            )
-        ),
+            ]
+        ],
 
-    ),
-);
+    ],
+];
